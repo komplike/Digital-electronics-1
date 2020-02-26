5890a221fc57159b06915bd6b894c145fb3fab7a
lab661 <lab661@PC-449.urel.feec.vutbr.cz>
2020-02-26 10:45:34 +0100

    04_lab

---
+++ Labs/02-ise/ise/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/santakremiec/Digital-electronics-1/Labs/02-ise/ise/top_tb00_isim_beh.exe" -prj "/home/lab661/Documents/santakremiec/Digital-electronics-1/Labs/02-ise/ise/top_tb00_beh.prj" "work.top_tb00" 
