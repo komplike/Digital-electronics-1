b491f8fd07ccb6d70164d3832673f2eefbb1d45a
komplike <romansan.tn@gmail.com>
2020-04-29 14:00:06 +0200

    update

--- Labs/code_lock/code_lock/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "/home/komplike/Digital-electronics-1/Labs/code_lock/code_lock/code_lock_tb_00_isim_beh.exe" -prj "/home/komplike/Digital-electronics-1/Labs/code_lock/code_lock/code_lock_tb_00_beh.prj" "work.code_lock_tb_00" 
